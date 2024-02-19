5af102aa93f6c5a18fc71db662579b662b5134ad
root <redarshubi@github.com>
2024-02-19 07:06:46 -0800

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" Script that fetches info about employee using jsonplaceholder """
+""" Script that uses JSONPlaceholder API to fetch info """
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    employee_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    
+    # Fetch employee data
+    user_url = '{}users/{}'.format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+    # Fetch TODO list
+    todos_url = '{}todos?userId={}'.format(base_url, employee_id)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for task in todos_data if task['completed'])
 
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+    # Print progress information
+    print("Employee {} is done with tasks ({}/{}):".format(user_data['name'], completed_tasks, total_tasks))
+    for task in todos_data:
+        if task['completed']:
+            print("\t{}".format(task['title']))
+
