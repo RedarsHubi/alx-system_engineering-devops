19c499e180bd3c35a5882d902bdd75434e0fc29c
root <redarshubi@github.com>
2024-03-05 05:09:21 -0800

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,6 +18,5 @@ def number_of_subscribers(subreddit):
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
         return 0
-    dic = res.json()
-    results = response.json().get("data")
+    results = res.json().get("data")
     return results.get("subscribers")
