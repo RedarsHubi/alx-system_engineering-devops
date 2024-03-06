67099f41d3a19a52e7617a8ea0346a5fc3025227
root <redarshubi@github.com>
2024-03-06 11:42:43 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
 file_line { 'fix_wp_settings_php':
   path    => '/var/www/html/wp-settings.php',
   line    => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
